680cdaf1f265d9c458fca382424664cb7b45f51d
Dilraj Singh <dilraj082@gmail.com>
2024-03-22 18:30:37 +0530

    initialized project

---
+++ script.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+link = "https://kbiz.kasikornbank.com/menu/maker-management/maker-history"
+
+class WebScraper():
+
+    def __init__(self) -> None:
+        pass
+
+    def call_scraper(self):
+        while True:
+            self.scraper()
+            time.sleep(5)
+
+    def scraper(self):
+        pass
